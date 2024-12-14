822407a93a1b018b09d126d5e564c1df518a3ef2
Shilpa <shilpa.pachipala@gmail.com>
2024-12-14 17:13:41 +0530

    my second commit

---
+++ src/basicprograms/secondroundofgit.java
@@ -0,0 +1,14 @@
+package basicprograms;
+
+public class secondroundofgit {
+
+	public static void main(String[] args) {
+		String s="shilpa";
+		StringBuffer sb=new StringBuffer(s);
+		StringBuffer reverse=sb.reverse();
+		System.out.println(reverse);
+		
+
+	}
+
+}
